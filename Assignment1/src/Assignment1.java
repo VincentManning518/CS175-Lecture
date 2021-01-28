0372e202afe5bf560287fd46223345286cfc70f5
vmann <vmann@DESKTOP-JD1DIRN>
2021-01-28 12:59:31 -0500

    Assignment 1

---
+++ Assignment1/src/Assignment1.java
@@ -0,0 +1,19 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1+2+3+4+5+6+7+8+9+10);
+		System.out.println("Matt");
+		System.out.println("Danny");
+		System.out.println("Zac");
+		System.out.println("   +");
+		System.out.println("  + +");
+		System.out.println(" +   +");
+		System.out.println("+-----+");
+		System.out.println("| .-. |");
+		System.out.println("| | | |");
+		System.out.println("+-+-+-+");
+	}
+
+}
