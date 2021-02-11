79fc8340ec1e6c0736c8559b40ac768c97d74c20
vmann <vmann@DESKTOP-JD1DIRN>
2021-02-11 10:15:34 -0500

    Assignment 2

---
+++ Assignment2/src/weatherStation.java
@@ -0,0 +1,27 @@
+
+public class weatherStation {
+
+	public static void main(String[] args) {
+		
+		String name = " Monmouth ";
+		double temp = 26.5;
+		int windSpeed = 13;
+		String windDirection = "north-northwest";
+		
+		System.out.print("The");
+		System.out.print(name);
+		System.out.println("Weather Station");
+		
+		System.out.print("Temperature: ");
+		System.out.print(temp);
+		System.out.println("°F");
+		
+		System.out.print("Wind: ");
+		System.out.print(windDirection);
+		System.out.print(" ");
+		System.out.print(windSpeed);
+		System.out.print(" km/h");
+		
+	}
+
+}
